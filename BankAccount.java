c67a606317b64c5b9bb0d99b1bc25decf5d40a1e
sadiqakausar <56330852+sadiqakausar@users.noreply.github.com>
2021-08-20 10:36:44 +0530

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,6 +1,6 @@
 package assessment;
 
 public interface BankAccount {
-void CheckBankBalance(int b);
-void validateUser(String user);
+public abstract void CheckBankBalance(int b);
+public abstract void validateUser(String user);
 }
